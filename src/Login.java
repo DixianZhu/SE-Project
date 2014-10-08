ade82e4f32528cc22d4ea937193e97528157d354
Dixian Zhu <394597519@qq.com>
2014-10-08 22:15:18 +0800

    Create Login.java

---
+++ src/Login.java
@@ -0,0 +1,85 @@
+import java.sql.*;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Login extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String passWord;
+	private String realName;
+	private String gender;
+	private String age;
+	private String location;
+	private String occupation;
+	private String hobby;
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+	public String getRealName(){
+		return realName;
+	}
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public String getAge(){
+		return age;
+	}
+	
+	public String getLocation(){
+		return location;
+	}
+	
+	public String getOccupation(){
+		return occupation;
+	}
+	
+	public String getHobby(){
+		return hobby;
+	}
+	
+	public String index() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String execute() throws Exception {
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/trip";
+			String user = "root";
+			String psw = "2121778";
+			conn = DriverManager.getConnection(url, user, psw);
+			if (!conn.isClosed())
+				System.out.println("Success connecting to the Database!");
+			else
+				System.out.println("Fail connecting to the Database!");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select * from member where user_name = '"
+					+ userName + "' and pass_word = '" + passWord + "'");
+			if(rs.next()){
+				realName=rs.getString("real_name");
+				gender=rs.getString("gender");
+				age=rs.getString("age");
+				location=rs.getString("location");
+				occupation=rs.getString("occupation");
+				hobby=rs.getString("hobby");
+				return SUCCESS;
+			}else{
+				return "wrong";
+			}
+		}catch (Exception e) {
+			System.out.print("connection error!");
+			e.printStackTrace();
+			return ERROR;
+		}
+	}
+}
